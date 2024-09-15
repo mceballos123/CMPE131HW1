a2fbe1d55355c43598cb4a5ef1b46ca0a1a8b18b
mceballos123 <martinceballos789@gmail.com>
2024-09-14 18:39:10 -0700

    Created a html, cs, js website that is a introduction assignment for my CMPE 131

--- scripts/webFront.js
+++ scripts/webFront.js
@@ -1,7 +1,4 @@
-// This function accepts a default password. However, if the user enters "FrankButt" on the screen,
-// the function will display "frankbutt" instead.
 
-//Next style the all elements displayed on the home page and the transcript page
 //let lowerCasePassword='frankbutt'
 let correctPassword='frankbutt';
 
